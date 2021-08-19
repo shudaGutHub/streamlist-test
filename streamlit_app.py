fbbf54c7c420be88e5ffe6331af5f54eec351e45
Saleem <saleem.huda@gmail.com>
2021-08-18 22:11:50 -0400

    Removed import os

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pathlib
 import pandas as pd
 import streamlit as st
-import os
+
 from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
 # from datetime import timedelta, datetime
 
@@ -17,7 +17,7 @@ BlackDiamond Risk Dashboard'''
 
 
 value_date = st.date_input('Value Date')
-pathTQ= os.path.join("C:\\Users\\Saleem\\OneDrive\\Documents\\blkd\\{}".format(value_date))
+pathTQ= pathlib.Path("C:\\Users\\Saleem\\OneDrive\\Documents\\blkd\\{}".format(value_date))
 
 fund = st.selectbox('Select Fund', ['BDEQ_Portfolio','BDOP_Portfolio','BDIN_Portfolio'])
 deriv = st.radio('Include Deriv',['Yes','No'])
