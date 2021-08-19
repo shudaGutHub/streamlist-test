5a8d302de7df7aa73d26bcac45c864b0e5ebc18b
Saleem <saleem.huda@gmail.com>
2021-08-18 22:00:26 -0400

    Commit test app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,131 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import os
+from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
+# from datetime import timedelta, datetime
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+from fractions import Fraction
+import matplotlib.pyplot as plt
+import seaborn as sns
+from s3fs.core import S3FileSystem
+import xlwings as xw
+import datetime as dt
+import yfinance as yf
+import openpyxl
+import scipy.stats as stats
+from collections import namedtuple
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+from openpyxl import workbook
+import quantstats as qs
+# %%
+import sys
+sys.path.insert(1,"C:\\Users\\Saleem\\projects\\X8MKB\\x8mkb")
+from BDFunds import get_option_data, load_s3_options
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# %%
+
+value_date = '2021-08-17'
+target_expiry = '2021-10-17'
+
+# %%
+
+# This is the bucket for price data
+# s3bucket = "s3://blkd/rshinydata/pricedata/"
+fps3_BDFund = "s3://blkd/rshinydata/summary/BDFundPortfolio.csv"
+fps3_Dash = "s3://blkd/rshinydata/summary/DashSummary.csv"
+# "s3://blkd/rshinydata/summary/{}".format(bdfund_filename)
+# BDFundPortfolio_filename = "https://blkd.s3.us-east-2.amazonaws.com/rshinydata/summary/BDFundPortfolio.csv"
+# DashSummary_filename = "https://blkd.s3.us-east-2.amazonaws.com/rshinydata/summary/DashSummary.csv"
+
+s3summary = "s3://blkd/rshinydata/summary/"
+fps3_BDCCSummary = os.path.join(s3summary, "BDCCSummary.csv")
+fps3_CPSummary = os.path.join(s3summary, "CPSummary.csv")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# dfDash = read_s3_csv(fps3_Dash)
+# dfBDCC = read_s3_csv(fps3_BDCCSummary)
+# dfCP = read_s3_csv(fps3_CPSummary)
+# dfDash = read_s3_csv(DashSummary_filename)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# %%
 
-    points_per_turn = total_points / num_turns
+def load_s3_equity(sym, dtype=None, sentinels=None, parse_dates=None):
+    fp = "s3://blkd/rshinydata/pricedata/{}.csv".format(sym)
+    s3 = S3FileSystem(anon=False)
+    try:
+        df = pd.read_csv(s3.open(fp, mode='rb'),
+                         dtype=dtype,
+                         na_values=sentinels,
+                         parse_dates=parse_dates, keep_date_col=True)
+    except:
+        print("No data: {}", sym)
+        return None
+    return df
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+
+# %% md
+
+# Get names for BDEQ
+
+
+# %%
+
+import csv
+
+
+def read_s3_csv(fp, dtype=None, sentinels=None, parse_dates=None, error_bad_lines=False):
+    # read df from s3
+    s3 = S3FileSystem(anon=False)
+    # fp = os.path.join('s3://blkd/rshinydata/pricedata/{}'.format(filename))  # df_breederscup.csv'
+    df = pd.read_csv(s3.open(fp, mode='rb'),
+                     engine='python',
+                     dtype=dtype,
+                     na_values=sentinels,
+                     parse_dates=parse_dates, keep_date_col=True, error_bad_lines=error_bad_lines
+                     )
+    return df
+
+
+# %%
+
+
+# %%
+
+dfDashS3 = read_s3_csv(fps3_Dash, dtype={'OptionExpiry': str, 'LastStockPriceDate': str, 'ZoneYYYYMMDD': str},
+                       sentinels={'OptionExpiry': ['0', 'NA', '']},
+                       parse_dates=['OptionExpiry', 'LastStockPriceDate', 'ZoneYYYYMMDD'], error_bad_lines=False)
+
+dfBDFunds = read_s3_csv(fps3_BDFund)   #"s3://blkd/rshinydata/summary/BDFundPortfolio.csv"
+dtype_bd = {'OptExpiryYYYYMMDD': str, 'Units': float}
+sentinels_bd = {'OptExpiryYYYYMMDD': ['0', 'NA', '']}
+parse_dates_bd = ['OptExpiryYYYYMMDD']
+
+# Magic commands implicitly `st.write()`
+''' _This_ is some __Markdown__
+BlackDiamond Risk Dashboard'''
+
+
+import pathlib
+import numpy as np
+
+value_date = st.date_input('Value Date')
+pathTQ= os.path.join("C:\\Users\\Saleem\\OneDrive\\Documents\\blkd\\{}".format(value_date))
+
+fund = st.selectbox('Select Fund', ['BDEQ_Portfolio','BDOP_Portfolio','BDIN_Portfolio'])
+deriv = st.radio('Include Deriv',['Yes','No'])
+data = pd.read_csv(pathlib.Path(pathTQ,fund+".csv"))
+st.dataframe(data)
+
+#prices = yf.download(tickers=symbol,period='1y')
+#st.line_chart(prices)
+#positions = data.query('Symbol = @symbol')['Units']
+#st.bar_chart(data=positions)
+
+
+
+    
+    
+    
