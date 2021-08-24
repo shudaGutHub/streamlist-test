57aae1df673c5688adbc504f4711d2fcd0a5d51a
Saleem <saleem.huda@gmail.com>
2021-08-24 12:31:59 -0400

    Merge remote-tracking branch 'origin/master' Use files in github repository for testing - Add BDOPPortfolio calculations - Update BDOP_Portfolio.csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,15 @@ import math
 import pathlib
 import pandas as pd
 import streamlit as st
+import sys
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+import pandas as pd
+
+from sklearn import cluster, covariance, manifold
 from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
 # from datetime import timedelta, datetime
 
@@ -12,17 +20,24 @@ from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
 
 
 # Magic commands implicitly `st.write()`
-''' _This_ is some __Markdown__
-BlackDiamond Risk Dashboard'''
+''' BlackDiamond Risk Dashboard'''
 
 
 value_date = st.date_input('Value Date')
-#pathTQ= pathlib.Path("C:\\Users\\Saleem\\OneDrive\\Documents\\blkd\\{}".format(value_date))
+
 #URL_DASH = "https://blkd.s3.us-east-2.amazonaws.com/rshinydata/summary/DashSummary.csv"
 fund = st.selectbox('Select Fund', ['BDEQ_Portfolio','BDOP_Portfolio','BDIN_Portfolio'])
 deriv = st.radio('Include Deriv',['Yes','No'])
 data = pd.read_csv(f"{fund}.csv")
+dfoptions = data[data.AssetType=="OP"].copy()
+dfequity = data[data.AssetType=="EQ"].copy()
+
+
 st.dataframe(data)
+
+
+
+
 
 #prices = yf.download(tickers=symbol,period='1y')
 #st.line_chart(prices)
