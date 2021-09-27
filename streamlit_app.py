67874c06a9d43d7978b2da86f578d0a030976969
Saleem <saleem.huda@gmail.com>
2021-09-27 15:50:16 -0400

    Integrate xlwings with python code Important to use the correct venv and xlwings 0.24.9

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,49 +13,23 @@ import sqlite3
 
 import pandas as pd
 
-from sklearn import cluster, covariance, manifold
+
 from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
 import sqlite3
 
 conn = sqlite3.connect("bdin.db")
-
-# from datetime import timedelta, datetime
-
-
-
-
-# Magic commands implicitly `st.write()`
-''' BlackDiamond Risk Dashboard'''
-
-
 value_date = st.date_input('Value Date')
 
 #URL_DASH = "https://blkd.s3.us-east-2.amazonaws.com/rshinydata/summary/DashSummary.csv"
 fund = st.selectbox('Select Fund', ['BDEQ_Portfolio','BDOP_Portfolio','BDIN_Portfolio','BDOP_DERIV'])
 
-data = pd.read_sql(con=conn, sql="SELECT * FROM BDFundPortfolio")
+data = pd.read_sql(con=conn, sql="SELECT * FROM  main_BDFundPortfolio")
 
 #symbols = list(data['Symbol'].unique())
 
 #dfequity = data[data.AssetType=="EQ"].copy()
 #risk = st.sidebar.multiselect("Risk Factor", symbols )
 
-st.dataframe(data.query("Portfolio == @fund & AssetType in ('EQ','OP')").set_index(["Symbol","Units"]))
+st.dataframe(data.query("Portfolio == @fund"))
 
 
-             
-st.sidebar.selectbox('Risk Contribution',)
-
-
-
-
-#prices = yf.download(tickers=symbol,period='1y')
-#st.line_chart(prices)
-#positions = data.query('Symbol = @symbol')['Units']
-#st.bar_chart(data=positions)
-
-
-
-    
-    
-    
