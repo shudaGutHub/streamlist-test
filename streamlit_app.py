0d4c1b11903ccbace7d4439faff943243c00c36a
shudaGutHub <saleem.huda@gmail.com>
2021-09-30 20:56:48 -0400

    Add load_option_prices_frm_S3() This loads each day's options files and writes a table to database

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ import sqlite3
 
 import pandas as pd
 
+import mibian
 
 from pandas import concat, DataFrame, read_csv, to_datetime, Series, MultiIndex
 import sqlite3
@@ -29,20 +30,106 @@ path_options = pathlib.Path("Z:\\rshinydata\\currentoptdata")
 path_equities = pathlib.Path("Z:\\rshinydata\\pricedata")
 
 
-
 conn = sqlite3.connect("bdin.db")
 value_date = st.date_input('Value Date')
 
-
-dfBDFunds = load_trades.load_BDFunds(load_trades.fp_BDFund,value_date)
 dfDashSummary = load_trades.load_symbols_from_dash().dropna(subset=['LongName'])
+fp_BDFund = pathlib.Path("Z:\\rshinydata\\summary\\BDFundPortfolio.csv")
+def load_BDFunds(fp=fp_BDFund, valdate=value_date):
+	"""Loads the BDFunds file from mounted S3"""
 
+	df = pd.read_csv(fp_BDFund, parse_dates=['OptExpiryYYYYMMDD'])
+	dfbd = df.assign(
+	Weight = df.CurrentWeight/100.0,
+	Symbol = df['Ticker'].map(lambda x: x.split(" ")[0]),
+	CountryCode = df['Ticker'].map(lambda x: x.split(" ")[1])
+			)
+
+	return dfbd.query('AssetType==["EQ","OP"]')
+dfBDFunds =load_BDFunds(fp_BDFund)
+
+
+sql_options = 'SELECT * FROM BDFunds_20210930 WHERE AssetType = "OP" AND Portfolio="BDOP_Portfolio"'
+pos_deriv = pd.read_sql(con=conn,sql=sql_options)
 
 symbols = dfDashSummary['Symbol']
 
-tables = {'DashSummary': dfDashSummary,'BDFundPortfolio':dfBDFunds}
+def get_options(df,
+                attr_price='PriceBase', attr_vol="HVOL",  start='2017-01-01', end='2021-09-14'):
+	dict_impliedVol = {}
+	dict_histVol = {}
+	option_models = {}
+	for row in df.itertuples():
+		assert row.AssetClass == "Option"
+
+		sym = row.Symbol
+		sigma = row.HVOL
+
+		spot = row.Close
+		trade_date = row[0][0] #TradeData (date)
+		ticker = row[0][1]   #Ticker
+		value_date = trade_date
+		print(value_date)
+		expiry = row.EXPIRY_DATE
+		number_of_days = (expiry - value_date).days
+		assert expiry >= value_date
+
+		strike = row.STRIKE
+		frate = row.RATE
+		qrate = row.RATE_Q
+		optPC = row.OptPC
+		bdfunds_ticker = None
+		option_models[ticker] = {'Symbol': row.Symbol,
+								 'ExpiryDate': row.EXPIRY_DATE,
+								 'Strike': row.STRIKE,
+								 'OptPC': row.OptPC}
+
+		bsmodel = lambda row: row[['Close','Strike','RATE','TERM_DAYS']]
+		bsdata = [row.Close,
+				  row.STRIKE, #This is a fixed number for the life of the contract
+				  row.RATE * 100,
+				  number_of_days]
+
+		print([sym, spot, strike,number_of_days,sigma])
+		idxThisDayAndTicker = row[0]
+		if optPC =="P":
+			model_from_price = mibian.BS(bsdata, putPrice=row.PriceBase)
+			model_from_hvol = mibian.BS(bsdata, volatility=row.HVOL * 100)
+			IVOL =model_from_price.impliedVolatility
+			PRICE_HVOL = model_from_hvol.putPrice
+			PRICE_IVOL = model_from_price.putPrice
+			DELTA_HVOL = model_from_hvol.putDelta
+			DELTA_IVOL = model_from_price.putDelta
+			TERM_DAYS = number_of_days
+			PROB_EXERCISE = 1.0 - mibian.BS(bsdata, volatility=row.HVOL*100).exerciceProbability
+			dict_histVol[idxThisDayAndTicker] =  [IVOL, PRICE_HVOL,PRICE_IVOL,DELTA_HVOL , DELTA_IVOL, TERM_DAYS,  PROB_EXERCISE]
+
+		elif optPC =="C":
+			# Get Implied Vol from callPrice
+			model_from_price = mibian.BS(bsdata, callPrice=row.PriceBase)
+			model_from_hvol = mibian.BS(bsdata, volatility=row.HVOL*100)
+			IVOL = model_from_price.impliedVolatility
+			PRICE_HVOL = model_from_hvol.callPrice
+			PRICE_IVOL = model_from_price.callPrice
+			DELTA_HVOL = model_from_hvol.callDelta
+			DELTA_IVOL = model_from_price.callDelta
+			TERM_DAYS = number_of_days
+			PROB_EXERCISE = mibian.BS(bsdata, volatility=row.HVOL*100).exerciceProbability
+
+			dict_histVol[idxThisDayAndTicker] = [IVOL, PRICE_HVOL, PRICE_IVOL, DELTA_HVOL, DELTA_IVOL, TERM_DAYS, PROB_EXERCISE]
+
+	computed_option_fields = ["IVOL", "PRICE_HVOL", "PRICE_IVOL", "DELTA_HVOL", "DELTA_IVOL", "TERM_DAYS",
+							  "PROB_EXERCISE"]
+	dictbs = {idx: model.__dict__ for idx, model in mibianBS.items()}
+	#dfbs = pd.DataFrame.from_dict(dictbs, orient='index').reset_index().rename(columns={'level_0':'TradeData', 'level_1':'Ticker'})
+	dfoptions = pd.DataFrame.from_dict(dict_histVol,orient='index', columns=computed_option_fields)
+	dfoptions.index = pd.MultiIndex.from_tuples(dfoptions.index.values, names=df.index.names)
+	return option_models, dfoptions
 
 
+
+
+#S([underlyingPrice, strikePrice, interestRate, daysToExpiration],
 def load_option_prices_from_S3(pathS3=path_options):
     """Load pathS3 options """
     # option_data = {f.replace(".csv",""): pd.read_csv(f) for f in os.listdir(path_options) if f not in options_bad_files}
